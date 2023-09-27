19339b10e0f63511a71c5b53f8cce893d2725132
ruben <rjulian@elpuig.xeill.net>
2023-09-28 00:52:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/fragments/OnBoarding1Fragment.java
@@ -0,0 +1,53 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link OnBoarding1Fragment newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OnBoarding1Fragment extends Fragment {
+    Button botonSiguiente;
+    Button botonFinal;
+    NavController navController;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_on_boarding1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        botonSiguiente = view.findViewById(R.id.botonSiguiente);
+        botonFinal = view.findViewById(R.id.botonFinal);
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onBoarding1Fragment_to_onBoardingFragment2);
+            }
+        });
+
+        botonFinal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_onBoarding1Fragment_to_homeFragment);
+            }
+        });
+    }
+}
